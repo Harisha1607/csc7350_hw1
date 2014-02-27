de48423ee85e8ad4f14ce57d14f68367db69e73b
Sandy <Sandy@10.0.0.9>
2014-02-27 09:53:12 +0530

    i added fastaReaderHelper class...I gave the file path in netbeans as arguments....Please check it

---
+++ HW1/src/hw1/UniqueSequences.java
@@ -0,0 +1,26 @@
+package hw1;
+import java.util.HashSet;
+import java.io.*;
+import java.util.*;
+import org.biojava3.core.sequence.io.*;
+import org.biojava3.core.sequence.ProteinSequence;
+
+public class UniqueSequences{
+
+    public static void main(String[] args) throws IOException, Exception{  
+
+            if(args.length != 1){  
+                    System.out.println("Usage: UniqueSequences <filename>");  
+                    System.exit(1);  
+            }  
+
+            System.out.println("Opening file " + args[0] + " ...");
+
+            LinkedHashMap<String, ProteinSequence> a;
+            a = FastaReaderHelper.readFastaProteinSequence(new File(args[0]));
+
+            HashSet<String> hs = new HashSet<>(a.keySet());
+            System.out.println(hs.size()+" unique sequences found!");
+
+    }
+}
